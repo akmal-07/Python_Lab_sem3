42183f9055859db1fb3283cf6a0f6a130564d961
Mohammed Akmal K.M. <188584648+akmal-07@users.noreply.github.com>
2025-10-16 11:46:20 +0530

    Add sine wave plot in graphs2.py
    Initial implementation of sine wave plot using matplotlib.

---
+++ graphs2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0 , 2*np.pi , 100)
+y = np.sin(x)
+y2 = np.cos(x)
+y3 = np.sin(x)*np.cos(x)
+plt.figure(figsize=(12,4))
+
+plt.subplot(1,3,1)
+plt.plot(x,y ,color = 'blue')
+plt.title('sine wave')
+plt.xlabel('X-axis')
+plt.ylabel('y-axis')
+     
