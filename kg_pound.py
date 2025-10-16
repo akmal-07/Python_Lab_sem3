6a15e5f0da25a93da36df6237a1f1a2fc49f0cc2
Mohammed Akmal K.M. <188584648+akmal-07@users.noreply.github.com>
2025-10-16 11:48:14 +0530

    Implement weight conversion in kg_pound.py
    Add weight conversion between kilograms and pounds.

---
+++ kg_pound.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the weight = "))
+k = int(input("enter 1 to convert into kgs and enter 2 for pounds = "))
+if(k==1) :
+    print(x/2.2)
+elif(k==2)  :
+    print(x*2.2) 
+else :
+    print("Enter valid converstion !")    
